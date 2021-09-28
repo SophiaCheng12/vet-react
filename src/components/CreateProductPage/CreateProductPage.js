dd445e09d83bb87d8da0ba7eb8a37bb29946525f
SophiaCheng12 <chiaoyin1520@gmail.com>
2021-09-28 14:23:06 +0800

    feat: add  InformationTotalBtn

--- src/components/CreateProductPage/CreateProductPage.js
+++ src/components/CreateProductPage/CreateProductPage.js
@@ -4,6 +4,8 @@ import CreateProductTitle from "./CreateProductTitle.js";
 import BasicInformation from "./BasicInformation.js";
 import OtherInformation from "./OtherInformation.js";
 import styles from "./CreateProductPage.module.css";
+import InformationTotalBtn from "./InformationTotalBtn.js";
+
 class CreateProductPage extends React.Component {
   render() {
     return (
@@ -14,12 +16,13 @@ class CreateProductPage extends React.Component {
           <div className={styles.main}>
             <form className={styles.form}>
               <div className="container-fluid" id={styles.informationForm}>
-                {/* informationForm */}
                 <div className={styles.row}>
                   <BasicInformation />
                   <OtherInformation />
                 </div>
               </div>
+
+              <InformationTotalBtn />
             </form>
           </div>
         </>
---
+++ src/components/CreateProductPage/InformationTotalBtn.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./CreateProductPage.module.css";
+
+class InformationTotalBtn extends React.Component {
+  render() {
+    return (
+      <div className={styles.informationTotalBtn}>
+        <button
+          type="button"
+          className={`btn ${styles.informationCreateBtn}  ${styles.informationButtonActive} `}
+        >
+          新增
+        </button>
+        <button type="button" className={`btn ${styles.informationCancelBtn}`}>
+          取消
+        </button>
+      </div>
+    );
+  }
+}
+
+export default InformationTotalBtn;
